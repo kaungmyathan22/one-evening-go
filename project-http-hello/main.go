167b79c0255522fa9e61ccd89919110e12e3dc4d
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-10-28 21:02:43 +0630

    update(): project hello finished

--- project-http-hello/main.go
+++ project-http-hello/main.go
@@ -15,5 +15,9 @@ func main() {
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
+	if name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 	fmt.Fprint(w, "Hello, ", name)
 }
