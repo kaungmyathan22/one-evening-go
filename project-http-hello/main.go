f6d7df17ce8f610cfcdd632e8ef53147f5f89741
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-10-28 21:43:49 +0630

    update(): http project finished

--- project-http-hello/main.go
+++ project-http-hello/main.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 )
 
+var calls = []string{}
+var stats = map[string]int{}
+
 func main() {
 	// Your solution goes here. Good luck!
 	http.HandleFunc("/hello", greet)
@@ -19,5 +22,16 @@ func greet(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	calls = append(calls, name)
+	count, ok := stats[name]
+	if !ok {
+		stats[name] = 1
+	} else {
+		stats[name] = count + 1
+	}
+	fmt.Printf("calls: %#v\n", calls)
+	fmt.Printf("stats: %#v\n\n", stats)
+
 	fmt.Fprint(w, "Hello, ", name)
+
 }
